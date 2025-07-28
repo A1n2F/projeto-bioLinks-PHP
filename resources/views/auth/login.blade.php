fd5f50458d95f8e1cd1472667c68f44d7e60718f
A1n2F <alissonfferreira22@gmail.com>
2025-07-28 16:06:48 -0300

    Criando login com Laravel

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,12 @@
 <div>
     <h1>LOGIN</h1>
 
-    <div></div>
+    <form action="/login" method="post">
+        @csrf
+
+        <input name="email" placeholder="Email" value="{{ old('email') }}" />
+        <input type="password" name="password" placeholder="Senha" />
+
+        <button>Logar</button>
+    </form>
 </div>

dee462789381d4faaf3cb68aaea2c6f29ebec36f
A1n2F <alissonfferreira22@gmail.com>
2025-08-04 14:20:33 -0300

    Criando a tela pública de links

---
+++ resources/views/bio-links.blade.php
@@ -0,0 +1,16 @@
+<div>
+    <h1>BIO LINKS</h1>
+    <img src="/storage/{{ $user->photo }}" alt="Photo Profile">
+    <h2>User {{ $user->name }} :: {{ $user->id }}</h2>
+    <h3>{{ $user->description }}</h3>
+
+    <ul>
+        @foreach ($user->links as $link)
+            <li> 
+                <a href="{{ $link->link }}" target="_blank">
+                    {{ $link->id }}. {{ $link->name }}
+                </a>
+            </li>
+        @endforeach
+    </ul>
+</div>
